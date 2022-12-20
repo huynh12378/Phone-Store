89e02a68b07c88e5e82b60bea03d8d8ac4e011bc
giotinhsaone1 <huynh741258@gmail.com>
2022-12-20 15:39:07 +0700

    Index Sign in,out profile Sanpham Chitietsanpham

---
+++ Ictshop/Controllers/SanphamController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class SanphamController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+
+        // GET: Sanpham
+        public ActionResult dtiphonepartial()
+        {
+            var ip = db.Sanphams.Where(n=>n.Mahang==2).Take(4).ToList();
+           return PartialView(ip);
+        }
+        public ActionResult dtsamsungpartial()
+        {
+            var ss = db.Sanphams.Where(n => n.Mahang == 1).Take(4).ToList();
+            return PartialView(ss);
+        }
+        public ActionResult dtxiaomipartial()
+        {
+            var mi = db.Sanphams.Where(n => n.Mahang == 3).Take(4).ToList();
+            return PartialView(mi);
+        }
+        public ActionResult ipad()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 4).Take(4).ToList();
+            return PartialView(ip);
+        }
+        public ActionResult tabletsamsung()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 8).Take(4).ToList();
+            return PartialView(ip);
+        }
+        public ActionResult tabletxiaomi()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 9).Take(4).ToList();
+            return PartialView(ip);
+        }
+        public ActionResult mac()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 10).Take(4).ToList();
+            return PartialView(ip);
+        }
+        public ActionResult dell()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 11).Take(4).ToList();
+            return PartialView(ip);
+        }
+        public ActionResult asus()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 12).Take(4).ToList();
+            return PartialView(ip);
+        }
+        //public ActionResult dttheohang()
+        //{
+        //    var mi = db.Sanphams.Where(n => n.Mahang == 5).Take(4).ToList();
+        //    return PartialView(mi);
+        //}
+        public ActionResult xemchitiet(int Masp=0)
+        {
+            var chitiet = db.Sanphams.SingleOrDefault(n=>n.Masp==Masp);
+            if (chitiet == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(chitiet);
+        }
+
+    }
+
+}
---
+++ Ictshop/Controllers/UserController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Ictshop.Models;
+namespace Ictshop.Controllers
+{
+    public class UserController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        // ĐĂNG KÝ
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+
+
+        // ĐĂNG KÝ PHƯƠNG THỨC POST
+        [HttpPost]
+        public ActionResult Dangky(Nguoidung nguoidung)
+        {
+            try
+            {
+                // Thêm người dùng  mới
+                db.Nguoidungs.Add(nguoidung);
+                // Lưu lại vào cơ sở dữ liệu
+                db.SaveChanges();
+                // Nếu dữ liệu đúng thì trả về trang đăng nhập
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Dangnhap");
+                }
+                return View("Dangky");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Dangnhap()
+        {
+            return View();
+
+        }
+        public ActionResult Profile()
+        {
+            var u = Session["use"] as Models.Nguoidung;
+            // trả về trang chi tiết người dùng
+            return View(u);
+        }
+        public ActionResult Edit()
+        {  //Kiểm tra đang đăng nhập
+
+            var u = Session["use"] as Models.Nguoidung;
+            return View(u);
+        }
+        public ActionResult Save()
+        {
+            Nguoidung kh = (Nguoidung)Session["use"];
+            db.SaveChanges();
+            return RedirectToAction("Profile");
+        }
+
+
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection userlog)
+        {
+            string userMail = userlog["userMail"].ToString();
+            string password = userlog["password"].ToString();
+            var islogin = db.Nguoidungs.SingleOrDefault(x => x.Email.Equals(userMail) && x.Matkhau.Equals(password));
+
+            if (islogin != null)
+            {
+                if (userMail == "Admin@gmail.com")
+                {
+                    Session["use"] = islogin;
+                    return RedirectToAction("Index", "Admin/Home");
+                }
+                else
+                {
+                    Session["use"] = islogin;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ViewBag.Fail = "Đăng nhập thất bại";
+                return View("Dangnhap");
+            }
+
+        }
+
+        public ActionResult DangXuat()
+        {
+            Session["use"] = null;
+            return RedirectToAction("Index", "Home");
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaNguoiDung,Hoten,Email,Dienthoai,Matkhau")] Nguoidung nguoidung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguoidung).State = EntityState.Modified;
+               db.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+            return View(nguoidung);
+        }
+    }
+}
